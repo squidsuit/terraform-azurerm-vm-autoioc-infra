a3e7170b5a3c6c7a6c9cd68209b6a91350ca5f9c
Robert Bell <rmbbell@gmail.com>
2021-07-10 15:03:14 +0000

    Updated min tls version to 1.2

--- storage.tf
+++ storage.tf
@@ -12,6 +12,7 @@ resource "azurerm_storage_account" "autoioc-sa" {
   location                 = azurerm_resource_group.autoioc-rg.location
   account_tier             = "Standard"
   account_replication_type = "GRS"
+  min_tls_version = "TLS1_2"
 
   tags = var.all-tags
 
